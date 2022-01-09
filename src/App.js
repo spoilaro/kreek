28be651e139e214fae44af6de846e22f7ecc0889
spoilar <johannes.haapanen@gmail.com>
2022-01-09 14:21:59 +0200

    Search word updates to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-
 import Navbar from "./components/Navbar";
 /* import Footer from "./components/Footer"; */
+import { useState } from "react";
 
 function App() {
+  const [searchWord, setSearchWord] = useState("");
+
   return (
     <div>
-      <Navbar/>
+      <Navbar searchFn={setSearchWord} />
     </div>
   );
 }
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 
-export default function Navbar(props) {
+export default function Navbar({ searchFn }) {
+  const [searchText, setSearchText] = useState("");
 
+  const handleChange = (e) => {
+    setSearchText(e.target.value);
+  };
 
-    return(
-        <div className="navBar">
-            <div className="searchBarDiv">
-                <form>
-                    <input class="searchBarInput"></input>
-                    <button class="searchBarButton">Etsi</button>
-                </form>
-            </div>
-        </div>
-    )
+  return (
+    <div className="navBar">
+      <div className="searchBarDiv">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            searchFn(searchText);
+          }}
+        >
+          <input
+            type="text"
+            className="searchBarInput"
+            value={searchText}
+            onChange={handleChange}
+          ></input>
+          <button className="searchBarButton" type="submit">
+            Etsi
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
