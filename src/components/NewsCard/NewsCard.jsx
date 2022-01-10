e25faf4d11c0d63f650bb6b9d179ddcc79f3a039
Leevi <joona.mlin@gmail.com>
2022-01-10 15:19:00 +0200

    Added not working RSS parsing for fixing

--- src/components/NewsCard/NewsCard.jsx
+++ src/components/NewsCard/NewsCard.jsx
@@ -1,14 +1,44 @@
-import React from 'react';
 import "./newsCard.css";
+import axios from "axios";
+import {XMLParser} from "fast-xml-parser"
+const func = async() => {
+    let newsList = [];
+    const res = await axios.get(
+        "https://cors-anywhere.herokuapp.com/https://feeds.yle.fi/uutiset/v1/recent.rss?publisherIds=YLE_UUTISET&concepts=18-35138",
+        {
+            responseType: "buffer",
+            resolveBodyOnly: true,
+            timeOut: 5000,
+            retry: 5,
+        }
+    );
 
+    const parser = new XMLParser();
+    let jObj = await parser.parse(res.data);
+
+    const items = jObj.rss.channel.item;
+    
+
+    items.forEach((elem) => {
+        const category_list = elem.category;
+        if (category_list.includes("koronavirus")) {
+            newsList.push(elem);
+        }
+    });
+    
+    return newsList;
+}
 export default function NewsCard() {
+    let newsList = async() => {await func()};
+    console.log(newsList[0])
+    const test = {title: "asdf"}
     return (
         <div className='newsCard'>
             <h1>Uutiset</h1>
             <ul className='list'>
-                <li>asdf</li>
-                <li>asdf</li>
-                <li>asdf</li>
+                
+                    
+            
             </ul>
         </div>
     )
