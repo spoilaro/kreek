869f076caba773f9983a64037be57be34171883c
spoilar <johannes.haapanen@gmail.com>
2022-01-10 17:45:16 +0200

    rss working

--- src/components/NewsCard/NewsCard.jsx
+++ src/components/NewsCard/NewsCard.jsx
@@ -1,45 +1,46 @@
 import "./newsCard.css";
 import axios from "axios";
-import {XMLParser} from "fast-xml-parser"
-const func = async() => {
-    let newsList = [];
-    const res = await axios.get(
-        "https://cors-anywhere.herokuapp.com/https://feeds.yle.fi/uutiset/v1/recent.rss?publisherIds=YLE_UUTISET&concepts=18-35138",
-        {
-            responseType: "buffer",
-            resolveBodyOnly: true,
-            timeOut: 5000,
-            retry: 5,
-        }
-    );
+import { XMLParser } from "fast-xml-parser";
+import { useState, useEffect } from "react";
 
-    const parser = new XMLParser();
-    let jObj = await parser.parse(res.data);
-
-    const items = jObj.rss.channel.item;
-    
-
-    items.forEach((elem) => {
-        const category_list = elem.category;
-        if (category_list.includes("koronavirus")) {
-            newsList.push(elem);
-        }
-    });
-    
-    return newsList;
-}
 export default function NewsCard() {
-    let newsList = async() => {await func()};
-    console.log(newsList[0])
-    const test = {title: "asdf"}
-    return (
-        <div className='newsCard'>
-            <h1>Uutiset</h1>
-            <ul className='list'>
-                
-                    
-            
-            </ul>
-        </div>
-    )
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    const loadArticles = async () => {
+      const parser = new XMLParser();
+
+      axios
+        .get(
+          "https://cors-anywhere.herokuapp.com/https://feeds.yle.fi/uutiset/v1/recent.rss?publisherIds=YLE_UUTISET&concepts=18-35138",
+          { headers: { Accept: "application/json" } }
+        )
+        .then((res) => parser.parse(res.data))
+        .then((data) => {
+          const parsed_items = [];
+          const items = data.rss.channel.item;
+          items.forEach((elem) => {
+            //elem.category.includes("koronavirus");
+            if (elem.category.includes("koronavirus")) {
+              parsed_items.push({ title: elem.title, link: elem.guid });
+              console.log(elem);
+            }
+          });
+          setArticles(parsed_items);
+        });
+    };
+    loadArticles();
+    //console.log(articles);
+  }, []);
+
+  const listArticles = articles.map((elem, key) => (
+    <li key={key}>{`Title: ${elem.title} and link: ${elem.link}`}</li>
+  ));
+
+  return (
+    <div className="newsCard">
+      <h1>Uutiset</h1>
+      <ul className="list">{listArticles}</ul>
+    </div>
+  );
 }
