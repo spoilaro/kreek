cf926c7d0e3b24e2e85421583fa8c316e8da1e8d
spoilar <johannes.haapanen@gmail.com>
2022-01-10 11:08:16 +0200

    Dropdown working

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -3,20 +3,27 @@ import "./style.css";
 import Dropdown from "react-dropdown";
 import "react-dropdown/style.css";
 import axios from "axios";
+import data from "../../fact_cov19cov.dimensions.json";
 
 export default function Navbar({ searchFn }) {
   const [searchText, setSearchText] = useState("Ahvenanmaa");
+  const places = data[0].children[0].children;
+
+  places.map((elem) => {
+    elem = elem.label;
+  });
 
   const options = ["Ahvenanmaa"];
 
   const handleChange = (option) => {
     setSearchText(option.label);
     searchFn(searchText);
+    console.log(data[0].children[0].children);
   };
 
   return (
     <div className="navBar">
-      <Dropdown options={options} onChange={handleChange} />
+      <Dropdown options={places} onChange={handleChange} />
     </div>
   );
 }
