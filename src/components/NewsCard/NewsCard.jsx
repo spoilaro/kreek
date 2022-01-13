8a1bf8538d35d904748972517a206121e1c57b1f
spoilar <johannes.haapanen@gmail.com>
2022-01-13 18:40:39 +0200

    Refactored callback hell

--- src/components/NewsCard/NewsCard.jsx
+++ src/components/NewsCard/NewsCard.jsx
@@ -9,35 +9,35 @@ export default function NewsCard() {
     const loadArticles = async () => {
       const parser = new XMLParser();
 
-      axios
-        .get(
-          "https://cors-anywhere.herokuapp.com/https://feeds.yle.fi/uutiset/v1/recent.rss?publisherIds=YLE_UUTISET&concepts=18-35138",
-          { headers: { Accept: "application/json" } }
-        )
-        .then((res) => parser.parse(res.data))
-        .then((data) => {
-          const parsed_items = [];
-          const items = data.rss.channel.item;
-          items.forEach((elem) => {
-            //elem.category.includes("koronavirus");
-            if (elem.category.includes("koronavirus")) {
-              parsed_items.push({ title: elem.title, link: elem.guid, date: new Date(elem.pubDate) });
-            }
+      const res = await axios.get(
+        "https://cors-anywhere.herokuapp.com/https://feeds.yle.fi/uutiset/v1/recent.rss?publisherIds=YLE_UUTISET&concepts=18-35138",
+        { headers: { Accept: "application/json" } }
+      );
+      const xml_parsed_data = parser.parse(res.data);
+      const parsed_items = [];
+      const items = xml_parsed_data.rss.channel.item;
+
+      items.forEach((elem) => {
+        if (elem.category.includes("koronavirus")) {
+          parsed_items.push({
+            title: elem.title,
+            link: elem.guid,
+            date: new Date(elem.pubDate),
           });
-          setArticles(parsed_items);
-        });
+        }
+      });
+      setArticles(parsed_items);
     };
     loadArticles();
-    //console.log(articles);
   }, []);
 
   const listArticles = articles.map((elem, key) => (
+    <li className="news-item" key={key}>
       <a className="news-link" href={elem.link}>
-        <li className="news-item" key={key}>
-          <h4 className="news-header">{elem.title}</h4>
-          <h5 className="news-date">{elem.date.toLocaleDateString("fi-FI")}</h5>
-        </li>
+        <h4 className="news-header">{elem.title}</h4>
+        <h5 className="news-date">{elem.date.toLocaleDateString("fi-FI")}</h5>
       </a>
+    </li>
   ));
 
   return (
